c18d4f8bb70841a2159bc605488838716a7fbcf2
brodieaustin <brodie.austin@gmail.com>
2015-10-16 13:15:39 -0500

    finally got gulp-sass working

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,35 @@
 var gulp = require('gulp');
 
 // Plugins
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var minifycss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 
 
-//var plumber = require('gulp-plumber');
-//var prefix = require('gulp-autoprefixer');
-//var uglify = require('gulp-uglify');
-//var imagemin = require('gulp-imagemin');
-//var livereload = require('gulp-livereload');
-//var lr = require('tiny-lr');
-//var server = lr();
+var config = {
+  sass_path: 'assets/scss',
+  css_path: 'assets/css',
+  js_src_path: 'assets/js/src',
+  js_path: 'assets/js',
+  bower_dir: 'bower_components'
+}
 
-// Compile Sass
-gulp.task('sass', function() {
-  gulp.src(['assets/scss/styles.scss'])
-    .pipe(sass({
-        loadPath: ['assets/scss', 'bower_components/foundation/scss', 'bower_components/foundation/scss/foundation']
-      }))
+gulp.task('sass', function () {
+    gulp.src(config.sass_path + '/*.scss')
+    .pipe(sass({ includePaths : [config.sass_path, config.bower_dir + '/foundation/scss', config.bower_dir + '/foundation/scss/foundation', config.bower_dir + '/foundation/scss/foundation/components']}).on('error', sass.logError))
+    //.pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
     .pipe(minifycss())
-    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest(config.css_path));
 });
 
 gulp.task('js', function(){
   gulp.src(['bower_components/bigSlide/dist/bigSlide.min.js', 'bower_components/mixitup/build/*.js'])
     .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('assets/js'))
+    .pipe(gulp.dest(config.js_path))
 })
 
 gulp.task('watch', function() {
   gulp.watch('assets/scss/*', ['sass'])
 });
 
-gulp.task('default', ['watch', 'sass', 'js']);
+gulp.task('default', ['sass', 'js', 'watch']);
