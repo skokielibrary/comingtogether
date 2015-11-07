86a118d221f0587729b295a728870bcad5ee6ada
brodieaustin <brodie.austin@gmail.com>
2015-11-06 22:59:50 -0600

    made it viva-fied

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-//based on https://git.anguiano.me/christian/gulp-foundation-5-template/
 
-// Gulp
 var gulp = require('gulp');
-
-// Plugins
+var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var cp = require('child_process');
 var concat = require('gulp-concat');
 
 
@@ -17,16 +16,40 @@ var config = {
   bower_dir: 'bower_components'
 }
 
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync', ['sass', 'js', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
 gulp.task('sass', function () {
     gulp.src(config.sass_path + '/*.scss')
     .pipe(sass({ includePaths : [config.sass_path, config.bower_dir + '/foundation/scss', config.bower_dir + '/foundation/scss/foundation', config.bower_dir + '/foundation/scss/foundation/components']}).on('error', sass.logError))
-    //.pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
+    .pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
     .pipe(minifycss())
+    .pipe(gulp.dest('_site/assets/css'))
+    .pipe(browserSync.reload({stream:true}))
     .pipe(gulp.dest(config.css_path));
 });
 
 gulp.task('js', function(){
-  gulp.src(['bower_components/bigSlide/dist/bigSlide.min.js', 'bower_components/mixitup/build/*.js'])
+  gulp.src(['bower_components/mixitup/build/*.js'])
     .pipe(concat('scripts.js'))
     .pipe(gulp.dest(config.js_path))
 })
@@ -35,4 +58,9 @@ gulp.task('watch', function() {
   gulp.watch('assets/scss/*', ['sass'])
 });
 
-gulp.task('default', ['sass', 'js', 'watch']);
+gulp.task('watch', function () {
+    gulp.watch('assets/scss/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', 'events/*', 'about/*', 'resources/*'], ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
