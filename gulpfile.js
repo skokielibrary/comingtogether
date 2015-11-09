960415f6013cb2ae8903e6557ac6705d44d7a64d
brodieaustin <brodie.austin@gmail.com>
2015-11-09 00:00:21 -0600

    added book and img folder to watch list

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('watch', function() {
 
 gulp.task('watch', function () {
     gulp.watch('assets/scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', 'events/*', 'about/*', 'resources/*'], ['jekyll-rebuild']);
+    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', 'events/*', 'about/*', 'books/*', 'resources/*', 'assets/img/*'], ['jekyll-rebuild']);
 });
 
 gulp.task('default', ['browser-sync', 'watch']);
